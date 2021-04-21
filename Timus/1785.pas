1737b20f2015c3262faaf2986aea31bdc2e36ca7
Core2DuoEver <75671497+Core2DuoEver@users.noreply.github.com>
2021-04-21 19:33:20 +0300

    Create 1785.pas
    First

---
+++ Timus/1785.pas
@@ -0,0 +1,16 @@
+var
+   a : integer;
+begin
+readln (a);
+case a of 
+  1..4 : writeln ('few');
+  5..9 : writeln ('several');
+  10..19 : writeln ('pack');
+  20..49 : writeln ('lots');
+  50..99 : writeln ('horde');
+  100..249 : writeln ('throng');
+  250..499 : writeln ('swarm');
+  500..999 : writeln ('zounds');
+  1000..2000 : writeln ('legion');
+  end;
+end.
